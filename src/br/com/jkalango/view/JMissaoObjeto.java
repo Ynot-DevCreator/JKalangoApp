3cf7bb4fbc6f3e2332a6c8c609bd2c5326f30a4f
Ynot-DevCreator <ead@academico.local>
2025-06-13 11:54:07 -0300

    fim aula sexta

---
+++ src/br/com/jkalango/view/JMissaoObjeto.java
@@ -0,0 +1,31 @@
+package br.com.jkalango.view;
+
+import javax.swing.JFrame;
+import br.com.jkalango.bo.IColor;
+
+public class JMissaoObjeto extends JFrame implements IColor {
+
+    @Override
+    public void mudarFundo() {
+        // Coloque a mecânica do jogo aqui!
+        
+    }
+
+    /*  O que é ?
+        Para que serve?
+        Como fazer?
+
+        Objeto
+        Classe
+        Construtor
+        Método
+        Herança
+        Atributos
+        Polimorfismo
+        Encapsulamento
+        Interfaces
+        Abstrato
+        Classe abstrato
+        Método Abstrato
+ */
+}
